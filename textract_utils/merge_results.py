dd057c680bd8918f0e9ccfb1d5e55d480fcfd9b7
your_github_username <your_github_email>
2025-01-23 04:32:35 +0530

    initial commit

--- textract_utils/merge_results.py
+++
@@ -1,5 +0,0 @@
-def combine_results(textract_text, tesseract_text):
-    textract_lines = set(textract_text.splitlines())
-    tesseract_lines = set(tesseract_text.splitlines())
-    combined_lines = textract_lines.union(tesseract_lines)
-    return "\n".join(combined_lines)
